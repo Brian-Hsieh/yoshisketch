c37555b6700d757cd8c0bfb050c405dd38e449ff
brian <brianhsieh38@gmail.com>
2025-03-04 11:57:09 +1100

    Added instructions
    - to be modified into collapsing nav bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,15 @@ export default function Home() {
       <div className="border border-gray-500 rounded-lg overflow-hidden">
         asdf
       </div>
+      <div className="mt-4 text-gray-600">
+        <p>Instructions:</p>
+        <ul className="list-disc pl-5">
+          <li>Drag with middle mouse button or CTRL+drag to pan</li>
+          <li>Use mouse wheel to zoom in/out</li>
+          <li>Draw with left mouse button</li>
+          <li>Press 'c' to clear canvas</li>
+        </ul>
+      </div>
     </div>
   );
 }
