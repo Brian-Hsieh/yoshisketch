8862d6049feb0d34b998c94e276df144698be523
brian <brianhsieh38@gmail.com>
2025-03-04 11:56:18 +1100

    Added canvas structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col items-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold mt-6 mb-4">Infinite Canvas</h1>
+      <div className="border border-gray-500 rounded-lg overflow-hidden">
+        asdf
+      </div>
+    </div>
+  );
 }
