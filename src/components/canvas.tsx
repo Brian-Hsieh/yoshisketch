067c4c109e4a053837817d849f01fbe5fa33fdc0
brian <brianhsieh38@gmail.com>
2025-03-10 17:12:11 +1100

    Add InfCanvas with drawing state and erase callback props

--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -2,11 +2,17 @@
 
 import React from "react";
 import dynamic from "next/dynamic";
+import type { DrawingState } from "./p5Wrapper";
 
 const P5Wrapper = dynamic(() => import("./p5Wrapper"), { ssr: false });
 
-const InfCanvas: React.FC = () => {
-  return <P5Wrapper />;
+interface Props {
+  drawingState: DrawingState;
+  onErase: (callback: () => void) => void;
+}
+
+const InfCanvas: React.FC<Props> = (props) => {
+  return <P5Wrapper {...props} />;
 };
 
 export default InfCanvas;
