0503d797dd1405d6196901b9a4bee840085f11d0
brian <brianhsieh38@gmail.com>
2025-03-10 17:49:28 +1100

    Set up drawing state management and erase function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+'use client';
+
+import React, { useState, useCallback } from "react";
 import InfCanvas from "../components/canvas";
+import Navbar from "../components/Navbar";
+import { DrawingMode, StrokeSize, TextSize, DrawingColor } from "../components/p5Wrapper";
 
 export default function Home() {
+  const [drawingState, setDrawingState] = useState({
+    mode: 'draw' as DrawingMode,
+    strokeSize: 'M' as StrokeSize,
+    textSize: 'M' as TextSize,
+    color: 'light-blue' as DrawingColor,
+    text: ''
+  });
+
+  const [eraseCanvas, setEraseCanvas] = useState(() => () => {});
+
   return (
     <div className="flex flex-col h-screen w-screen overflow-hidden bg-gray-100">
       <div className="flex-grow relative">
