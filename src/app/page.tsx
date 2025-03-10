4bcc81f02fc0a4e61e786206c21d6c879957ff0b
brian <brianhsieh38@gmail.com>
2025-03-10 17:22:20 +1100

    Deleted drawing instructions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,6 @@ import InfCanvas from "../components/canvas";
 
 export default function Home() {
   return (
-      <div className="mt-4 text-gray-600">
-        <p>Instructions:</p>
-        <ul className="list-disc pl-5">
-          <li>Drag with middle mouse button or CTRL+drag to pan</li>
-          <li>Use mouse wheel to zoom in/out</li>
-          <li>Draw with left mouse button</li>
-          <li>Press 'c' to clear canvas</li>
-        </ul>
     <div className="flex flex-col h-screen w-screen overflow-hidden bg-gray-100">
       <div className="flex-grow relative">
         <InfCanvas 
